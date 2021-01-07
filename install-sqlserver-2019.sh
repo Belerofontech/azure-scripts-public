cd7b2fd1e61f5b372ccff1c90b7496edf0b61412
pachi-belero <pachi.sucunza@belerofontech.com>
2021-01-07 23:08:00 +0100

    Added "main" user creation (different from SA), and some more checks, messages and configurability

--- install-sqlserver-2019.sh
+++ install-sqlserver-2019.sh
@@ -4,6 +4,7 @@
 # and https://docs.microsoft.com/en-us/sql/linux/sample-unattended-install-ubuntu?view=sql-server-linux-ver15
 #
 # NOTE: using this script implies accepting the license (EULA). See ACCEPT_EULA in the doc and below
+# NOTE: requires having run previously "install-sqlserver-odbc-driver.sh" (installs sqlcmd -- package mssql-tools)
 # NOTE: does NOT work on Ubuntu in WSL!
 
 # Optional: more debug info (show executed commands)
@@ -14,11 +15,14 @@
 # MAINUSER is the preferred/expected var to get the desired value from outside; SUDO_USER can be used also,
 # in case MAINUSER is not defined. If none of the two are defined/available, set a default value: "belero"
 [[ -z "$MAINUSER" ]] && [[ ! -z "$SUDO_USER" ]] && [[ "root" != "$SUDO_USER" ]] && MAINUSER="$SUDO_USER"
-[[ -z "$MAINUSER" ]] && MAINUSER="belero"
+[[ -z "$MAINUSER" ]] && MAINUSER="belero"  # Our default value, if not set
 echo "Value for MAINUSER variable: $MAINUSER"
 
-[[ -z "$DBUSER" ]] && DBUSER="$MAINUSER"
+[[ -z "$DBUSER" ]] && DBUSER="$MAINUSER"  # Same as the other var, if not set
 echo "Value for DBUSER variable: $DBUSER"
+
+[[ -z "$DBLICENSE" ]] && DBLICENSE="express"  # Our default value, if not set
+echo "Value for DBLICENSE variable: $DBLICENSE"
 
 if [[ -z "$DBPASS" ]]
 then
@@ -56,6 +60,17 @@ fi
 if insideWSL
 then
     echo "Microsoft SQL Server 2019 is not supported on WSL"
+    exit 1
+fi
+
+# Check for the pre-requisite: sqlcmd
+echo
+echo "Checking the availability of the pre-requisite command 'sqlcmd'"
+if [[ ! -x /opt/mssql-tools/bin/sqlcmd ]]
+then
+    echo "Error, pre-requisite command 'sqlcmd' not found"
+    echo "Install the mssql-tools package (bcp and sqlcmd), for instance by running:"
+    echo "  sudo ./install-sqlserver-odbc-driver.sh"
     exit 1
 fi
 
@@ -104,6 +119,10 @@ curl -sf https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 # Get the updated repository information (in this case it must be done always, since we have added a new repository)
 apt-get -y update
 
+# Stop the service (just in case this script is run again after a successful install)
+systemctl stop mssql-server --no-pager
+sleep 5
+
 echo
 echo "Installing packages from Microsoft repository"
 ACCEPT_EULA=Y apt-get -y install mssql-server
@@ -124,11 +143,18 @@ echo "  sudo /opt/mssql/bin/mssql-conf setup"
 # MSSQL_PID: Product ID of the version of SQL server you're installing
 #   Must be evaluation, developer, express, web, standard, enterprise, or your 25 digit product key
 # MSSQL_SA_PASSWORD: Password for the SA user (required)
-ACCEPT_EULA=Y MSSQL_SA_PASSWORD=$DBPASS MSSQL_PID=express /opt/mssql/bin/mssql-conf -n setup accept-eula
+#   For that, we generate a random password that we will NOT store or show...!
+
+DBPASSSA=$( base64 -w20 /dev/urandom 2>/dev/null | grep -e "+\|/" | head -n1 2>/dev/null )
+[[ -z "$DBPASSSA" ]] && echo "Error generating random SA password" && exit 1
+
+ACCEPT_EULA=Y MSSQL_SA_PASSWORD="$DBPASSSA" MSSQL_PID="$DBLICENSE" /opt/mssql/bin/mssql-conf -n setup accept-eula
 
 echo
-echo "Optional (recommended): install mssql-tools package (bcp and sqlcmd), for instance by running:"
-echo "  sudo install-sqlserver-odbc-driver.sh"
+echo "NOTE: to change the SA password afterwards, use (with the service stopped):"
+echo "  sudo /opt/mssql/bin/mssql-conf set-sa-password"
+
+echo
 echo "Also, remember to configure firewall(s) to allow traffic to TCP port 1433..."
 # Setup Ubuntu local firewall (disabled by default)
 # sudo ufw allow 1433/tcp
@@ -141,12 +167,37 @@ echo "Also, remember to configure firewall(s) to allow traffic to TCP port 1433.
 # apt-get -y install mssql-server-agent
 
 echo
-echo "Restarting SQL Server..."
+echo "Restarting SQL Server after set-up"
 systemctl restart mssql-server --no-pager
 sleep 5
 
 # service mssql-server status | cat
 systemctl status mssql-server --no-pager
+
+echo
+echo "Checking SQL Server databases"
+sleep 5
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "$DBPASSSA" \
+    -Q "SELECT Name from sys.databases"
+[[ $? -ne 0 ]] && echo "Warning, the previous command failed, maybe the server is not ready? Waiting a bit..." && sleep 10
+
+echo
+echo "Creating superuser '$DBUSER'"
+sleep 5
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "$DBPASSSA" \
+    -Q "CREATE LOGIN [$DBUSER] WITH PASSWORD=N'$DBPASS', DEFAULT_DATABASE=[master], CHECK_EXPIRATION=ON, CHECK_POLICY=ON; ALTER SERVER ROLE [sysadmin] ADD MEMBER [$DBUSER]"
+[[ $? -ne 0 ]] && echo "Error in previous command, please check and create the user manually!" && exit 1
+
+echo
+echo "Checking SQL Server users"
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "$DBPASSSA" \
+    -Q "SELECT name FROM sys.server_principals"
+
+# Just in case, because we don't want to keep it...
+unset DBPASSSA
+
+# For info about troubleshooting possible problems, see:
+# https://docs.microsoft.com/es-es/sql/linux/sql-server-linux-troubleshooting-guide?view=sql-server-ver15
 
 # # Optional: use this to force output to be shown, when run remotely on Azure with "run-custom-script.sh" (making the script exit status != 0 means that it didn't finish successfully)
 # echo "FINISHED. Now will end script execution with error status 101..." 1>&2
