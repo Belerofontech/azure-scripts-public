d3d645ea92efcca1577e60a602c67b7407af8ced
pachi-belero <pachi.sucunza@belerofontech.com>
2021-01-07 11:27:30 +0100

    More VM init scripts

---
+++ install-aa-basics.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# Script to install the desired basic packages and do the initial setup on a Ubuntu (18.04) system
+#
+# Based on the previous cloud-config.txt file (runcmd section) from the former "vm-ubuntuserver-1804" template (...)
+
+echo
+echo "BELEROFONTECH - STARTING CUSTOM 'BASIC' INIT!"
+
+# Optional: more debug info (show executed commands)
+# set -x
+
+# Define default variable values if missing
+
+# MAINUSER is the preferred/expected var to get the desired value from outside; SUDO_USER can be used also,
+# in case MAINUSER is not defined. If none of the two are defined/available, set a default value: "belero"
+[[ -z "$MAINUSER" ]] && [[ ! -z "$SUDO_USER" ]] && [[ "root" != "$SUDO_USER" ]] && MAINUSER="$SUDO_USER"
+[[ -z "$MAINUSER" ]] && MAINUSER="belero"
+echo "Value for MAINUSER variable: $MAINUSER"
+
+# Check for root permissions (user id = 0)
+if [[ $( id -u ) != 0 ]]
+then
+    echo "This script must be run with root privileges"
+    exit 1
+fi
+
+# Avoid apt-get commands to ask config/setup questions interactively (Debian/Ubuntu)
+export DEBIAN_FRONTEND=noninteractive
+
+sh -xc 'date ; env ; last ; w ; whoami ; pwd'
+
+# More secure permissions for main user's HOME dir
+chmod 0750 /home/$MAINUSER
+
+apt-get -y update && apt-get -y -u dist-upgrade
+# General system tools/utilities packages that we want to be always installed
+apt-get -y install apt-transport-https ca-certificates curl cifs-utils dos2unix colorized-logs ccze byobu gdb zip unzip bc vim neofetch
+# Pyhton-specific packages
+apt-get -y install python3-pip python3-venv python3-wheel unixodbc-dev mdbtools
+# # Optional: install Cbc and GLPK solvers
+# apt-get -y install coinor-cbc glpk-utils
+
+# Remove some of the default system info shown at each login
+apt-get -y remove landscape-common
+systemctl disable motd-news.timer
+chmod -x /etc/update-motd.d/00-header
+chmod -x /etc/update-motd.d/10-help-text
+chmod -x /etc/update-motd.d/50-motd-news
+chmod -x /etc/update-motd.d/91-release-upgrade
+# More "drastic" alternative to the previous lines:
+#   rm -f /etc/update-motd.d/00-header /etc/update-motd.d/10-help-text /etc/update-motd.d/50-motd-news /etc/update-motd.d/91-release-upgrade
+
+# # Optional: Remove Python 2 and set Python 3 as the default for scripts that use just "python". From:
+# # https://stackoverflow.com/a/50331137
+# apt-get -y remove python2.7 python2.7-minimal python-minimal
+# # TO-DO: check, the following line is not needed really...
+# # update-alternatives --remove python /usr/bin/python2
+# update-alternatives --install /usr/bin/python python /usr/bin/python3 10
+# update-alternatives --set python /usr/bin/python3
+
+echo
+echo "BELEROFONTECH - FINISHED CUSTOM 'BASIC' INIT!"
+
+# # Optional: use this to force output to be shown, when run remotely on Azure with "run-custom-script.sh" (making the script exit status != 0 means that it didn't finish successfully)
+# echo "FINISHED. Now will end script execution with error status 101..." 1>&2
+# exit 101
---
+++ install-pip-packages.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+# Script to install Python (3.x) packages with pip on a Ubuntu (18.04) system
+#
+# Based on the previous cloud-config.txt file (runcmd section) from the former "vm-ubuntuserver-1804" template (...)
+
+echo
+echo "BELEROFONTECH - STARTING CUSTOM 'PYTHON PIP' INIT!"
+
+# Optional: more debug info (show executed commands)
+# set -x
+
+# Define default variable values if missing
+
+# MAINUSER is the preferred/expected var to get the desired value from outside; SUDO_USER can be used also,
+# in case MAINUSER is not defined. If none of the two are defined/available, set a default value: "belero"
+[[ -z "$MAINUSER" ]] && [[ ! -z "$SUDO_USER" ]] && [[ "root" != "$SUDO_USER" ]] && MAINUSER="$SUDO_USER"
+[[ -z "$MAINUSER" ]] && MAINUSER="belero"
+echo "Value for MAINUSER variable: $MAINUSER"
+
+# Get the updated repository information
+function OptionalAptGetUpdate()
+{
+    # Run only if "update-success-stamp" file is present and older than 1 hour (means apt-get update has been run recently, and no apt-get clean afterwards)
+    LAST_UPDATED=$( stat --format="%X" /var/lib/apt/periodic/update-success-stamp 2>/dev/null || echo 0 )
+    TIME_DIFF=$(( $( date +%s ) - LAST_UPDATED ))
+    [[ $TIME_DIFF -gt 3600 ]] && apt-get -y update
+}
+
+# Check for root permissions (user id = 0)
+if [[ $( id -u ) != 0 ]]
+then
+    echo "This script must be run with root privileges"
+    exit 1
+fi
+
+# Avoid apt-get commands to ask config/setup questions interactively (Debian/Ubuntu)
+export DEBIAN_FRONTEND=noninteractive
+
+sh -xc 'date ; whoami ; pwd'
+
+# Install pre-requisite packages and get the updated repository information
+PREREQ="apt-transport-https python3-pip python3-venv python3-wheel unixodbc-dev mdbtools"
+echo
+echo "Installing pre-requisite package(s): $PREREQ"
+# Run apt-get update only if it hasn't run recently, to save some time
+OptionalAptGetUpdate
+apt-get -y install $PREREQ
+[[ $? -ne 0 ]] && echo "Error, pre-requisite package(s) cannot be installed" && exit 1
+
+echo
+# Optional: update Python pip (but not to 20.3 version or later, which has big changes in dependency management!)
+sudo -E -u $MAINUSER python3 -m pip install --user --upgrade 'pip<20.3'
+
+echo
+# Install desired Python packages with pip (user mode!)
+sudo -E -u $MAINUSER python3 -m pip install --user pandas pyomo xlwt xlrd openpyxl matplotlib pillow scipy statsmodels xlsxwriter pyodbc sqlalchemy pymysql pandas_access
+# TO-DO: check; this package fails to install!
+# sudo -E -u $MAINUSER python3 -m pip install --user streamlit
+# TO-DO: check; this package fails to install!
+# sudo -E -u $MAINUSER python3 -m pip install --user pandas_profiling
+
+# Check that all dependencies are OK
+echo
+sudo -E -u $MAINUSER python3 -m pip check
+
+echo
+echo "BELEROFONTECH - FINISHED CUSTOM 'PYTHON PIP' INIT!"
+
+# # Optional: use this to force output to be shown, when run remotely on Azure with "run-custom-script.sh" (making the script exit status != 0 means that it didn't finish successfully)
+# echo "FINISHED. Now will end script execution with error status 101..." 1>&2
+# exit 101
