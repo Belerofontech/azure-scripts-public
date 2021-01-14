e3baa5e0a37a7bf34b6a0ff01f06c146397864aa
pachi-belero <pachi.sucunza@belerofontech.com>
2021-01-14 09:44:11 +0100

    IMPORTANT: script no longer can be used by "normal" (non-root) users

--- install-ab-git-azure-scripts.sh
+++ install-ab-git-azure-scripts.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 # Script to install (clone/download) the GitHub repository with these scripts (azure-scripts-public) on a (Ubuntu) system
 #
-# NOTE: this script can also be used by a normal (non-root) user
-# NOTE: in that case, it will not write its output to /var/log (...)
+# NOTE: this script no longer can be used by "normal" (non-root) users (...)
 
 # Define default variable values if missing
-# NOTE: in this case, only MAINUSER can be defined/used, and only if runnig as root
 
-# In this script being root is not a requirement, but the behaviour changes a bit
+# MAINUSER is the preferred/expected var to get the desired value from outside; SUDO_USER can be used also,
+# in case MAINUSER is not defined. If none of the two are defined/available, set a default value: "belero"
+[[ -z "$MAINUSER" ]] && [[ ! -z "$SUDO_USER" ]] && [[ "root" != "$SUDO_USER" ]] && MAINUSER="$SUDO_USER"
+[[ -z "$MAINUSER" ]] && MAINUSER="belero"
+echo "Value for MAINUSER variable: $MAINUSER"
+
+# Check for root permissions (user id = 0)
 if [[ $( id -u ) != 0 ]]
 then
-    # The current user (the one that executes the script) should be in the USER env. variable
-    unset MAINUSER && [[ ! -z "$USER" ]] && [[ "root" != "$USER" ]] && MAINUSER="$USER"
-    [[ -z "$MAINUSER" ]] && echo "Error, cannot automatically set the value for MAINUSER variable. Try using sudo?" && exit 1
-else
-    # MAINUSER is the preferred/expected var to get the desired value from outside; SUDO_USER can be used also,
-    # in case MAINUSER is not defined. If none of the two are defined/available, set a default value: "belero"
-    [[ -z "$MAINUSER" ]] && [[ ! -z "$SUDO_USER" ]] && [[ "root" != "$SUDO_USER" ]] && MAINUSER="$SUDO_USER"
-    [[ -z "$MAINUSER" ]] && MAINUSER="belero"
+    echo "This script must be run with root privileges"
+    exit 1
 fi
 
 echo "Value for MAINUSER variable: $MAINUSER"
