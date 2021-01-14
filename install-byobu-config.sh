1997e9c0b2dba90f6a90ee8d734eb11c9585171f
pachi-belero <pachi.sucunza@belerofontech.com>
2021-01-14 09:43:57 +0100

    IMPORTANT: script no longer can be used by "normal" (non-root) users. Also: small message and .profile management improvements

--- install-byobu-config.sh
+++ install-byobu-config.sh
@@ -4,42 +4,45 @@
 # Based on the previous cloud-config.txt file (runcmd and write_files sections) from the former "vm-ubuntuserver-1804" template (...)
 #
 # NOTE: the byobu config is applied to ONE user only!
-#
-# NOTE: this script can also be used by a normal (non-root) user
-# NOTE: in that case, it will not write its output to /var/log (...)
+# NOTE: this script no longer can be used by "normal" (non-root) users (...)
 
 # Define default variable values if missing
-# NOTE: in this case, only MAINUSER can be defined/used, and only if runnig as root
 
-# In this script being root is not a requirement, but the behaviour changes a bit
+# MAINUSER is the preferred/expected var to get the desired value from outside; SUDO_USER can be used also,
+# in case MAINUSER is not defined. If none of the two are defined/available, set a default value: "belero"
+[[ -z "$MAINUSER" ]] && [[ ! -z "$SUDO_USER" ]] && [[ "root" != "$SUDO_USER" ]] && MAINUSER="$SUDO_USER"
+[[ -z "$MAINUSER" ]] && MAINUSER="belero"
+echo "Value for MAINUSER variable: $MAINUSER"
+
+# Check for root permissions (user id = 0)
 if [[ $( id -u ) != 0 ]]
 then
-    # The current user (the one that executes the script) should be in the USER env. variable
-    unset MAINUSER && [[ ! -z "$USER" ]] && [[ "root" != "$USER" ]] && MAINUSER="$USER"
-    [[ -z "$MAINUSER" ]] && echo "Error, cannot automatically set the value for MAINUSER variable. Try using sudo?" && exit 1
+    echo "This script must be run with root privileges"
+    exit 1
+fi
 
-    # Configure and auto-activate byobu at login
+echo
+echo "BELEROFONTECH - STARTING CUSTOM 'BYOBU CONFIG AND ENVIRONMENT' INIT!"
+
+# Leave trace of this script's output. See: https://unix.stackexchange.com/a/145654
+exec &> >(tee -a /var/log/belero-install-scripts.log)
+chmod -f o-rwx /var/log/belero-install-scripts.log
+
+# Optional: more debug info (show executed commands)
+# set -x
+
+sh -xc 'date ; env ; whoami ; pwd'
+
+# Configure and auto-activate byobu at login: add it to .profile (only if not already done)
+# TO-DO: maybe do it for all users, in /etc/profile.d/... instead?
+grep -q '# Belerofontech - Help before launching byobu. Can be skipped with ENTER"' /home/$MAINUSER/.profile
+if [[ $? -eq 1 ]]
+then
     echo | tee -a /home/$MAINUSER/.profile
     echo '# Belerofontech - Help before launching byobu. Can be skipped with ENTER' | tee -a /home/$MAINUSER/.profile
-    echo 'read -s -t 5 -p "BELEROFONTECH - BYOBU. F1: help (PuTTY: keyb. cfg. \"Xterm R6\") Alt-F12: mouse!"' | tee -a /home/$MAINUSER/.profile
-    byobu-enable
-    byobu-enable-prompt
-    byobu-ctrl-a emacs
-    # Run at least a simple command inside byobu once, so that it will create all its config files
-    byobu -c "ls -la /home/$MAINUSER/.byobu"
-else
-    # Running as root
-
-    # MAINUSER is the preferred/expected var to get the desired value from outside; SUDO_USER can be used also,
-    # in case MAINUSER is not defined. If none of the two are defined/available, set a default value: "belero"
-    [[ -z "$MAINUSER" ]] && [[ ! -z "$SUDO_USER" ]] && [[ "root" != "$SUDO_USER" ]] && MAINUSER="$SUDO_USER"
-    [[ -z "$MAINUSER" ]] && MAINUSER="belero"
-    echo "Value for MAINUSER variable: $MAINUSER"
-
-    # Configure and auto-activate byobu at login
-    echo | tee -a /home/$MAINUSER/.profile
-    echo '# Belerofontech - Help before launching byobu. Can be skipped with ENTER' | tee -a /home/$MAINUSER/.profile
-    echo 'read -s -t 5 -p "BELEROFONTECH - BYOBU. F1: help (PuTTY: keyb. cfg. \"Xterm R6\") Alt-F12: mouse!"' | tee -a /home/$MAINUSER/.profile
+    echo 'echo ; echo "BELEROFONTECH - BYOBU. F1: help; F7: output mgmt., then / for search; F9: cfg."' | tee -a /home/$MAINUSER/.profile
+    echo 'read -s -t 5 -p "  PuTTY: keyb. cfg. if needed: \"Xterm R6\"; Alt-F12: mouse mode     (ENTER: OK)" ; echo' | tee -a /home/$MAINUSER/.profile
+    # Set correct permissions, just in case
     chown $MAINUSER:$MAINUSER /home/$MAINUSER/.profile
     chmod 0644 /home/$MAINUSER/.profile
     sudo -E -u $MAINUSER byobu-enable
@@ -48,21 +51,6 @@ else
     # Run at least a simple command inside byobu once, so that it will create all its config files
     sudo -E -u $MAINUSER byobu -c "ls -la /home/$MAINUSER/.byobu"
 fi
-
-echo
-echo "BELEROFONTECH - STARTING CUSTOM 'BYOBU CONFIG AND ENVIRONMENT' INIT!"
-
-if [[ $( id -u ) = 0 ]]
-then
-    # Leave trace of this script's output. See: https://unix.stackexchange.com/a/145654
-    exec &> >(tee -a /var/log/belero-install-scripts.log)
-    chmod -f o-rwx /var/log/belero-install-scripts.log
-fi
-
-# Optional: more debug info (show executed commands)
-# set -x
-
-sh -xc 'date ; env ; whoami ; pwd'
 
 # Modify byobu key-bindings config file to use CTRL-B as prefix (tmux default)
 cat > /home/$MAINUSER/.byobu/keybindings.tmux << EOF
